dd18cf47fd82233db5e1a36247ea37a0a3d33101
Aditya Katiyar <adityakatiyar81@gmail.com>
2026-01-13 05:39:34 +0530

    Update formula to v2.1.1

--- Formula/agentk8.rb
+++ Formula/agentk8.rb
@@ -4,8 +4,8 @@
 class Agentk8 < Formula
   desc "Multi-Agent Claude Code Terminal Suite"
   homepage "https://github.com/de5truct0/agentk"
-  url "https://registry.npmjs.org/agentk8/-/agentk8-2.1.0.tgz"
-  sha256 "5264fccfb539dd909b1335dc997c96fd361df101"
+  url "https://registry.npmjs.org/agentk8/-/agentk8-2.1.1.tgz"
+  sha256 "59a82063dc6a16fef13948751c445b829609611e"
   license "MIT"
 
   depends_on "node"
@@ -31,6 +31,6 @@ class Agentk8 < Formula
   end
 
   test do
-    assert_match "2.1.0", shell_output("#{bin}/agentk --version")
+    assert_match "2.1.1", shell_output("#{bin}/agentk --version")
   end
 end
